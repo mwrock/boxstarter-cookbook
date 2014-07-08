1cc9ba19a068ce7838a30a293d6b94beda56cf54
mwrock <matt@mattwrock.com>
2014-07-08 16:51:29 -0700

    bumping to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'matt@mattwrock.com'
 license          'Apache 2.0'
 description      'Executes chocolatey packages and adhoc powershell in a reboot resilient context'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'windows' 	
