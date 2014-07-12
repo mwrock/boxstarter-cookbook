5fdc6d14bfb3321aba7888b5a4f40729aaee4fbf
mwrock <matt@mattwrock.com>
2014-07-11 23:52:04 -0700

    exit boxstarter install if the boxstarter temp dir exists

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,15 @@
 return 'platform not supported' if node['platform_family'] != 'windows'
 
+return if ::Dir.exist?(node['boxstarter']['tmp_dir'])
+
 directory node['boxstarter']['tmp_dir']
 
 cookbook_file "bootstrapper.ps1" do
-  path "#{ENV['TEMP']}/boxstarter/bootstrapper.ps1"
+  path "#{node['boxstarter']['tmp_dir']}/bootstrapper.ps1"
   action :create
 end
 
-template "#{ENV['TEMP']}/boxstarter/setup.bat" do
+template "#{node['boxstarter']['tmp_dir']}/setup.bat" do
   source "ps_wrapper.erb"
   variables({
     :command => "-command \". '%~dp0bootstrapper.ps1';Get-Boxstarter -force\""
@@ -15,6 +17,5 @@ template "#{ENV['TEMP']}/boxstarter/setup.bat" do
 end
 
 execute 'install boxstarter' do
-  command "#{ENV['TEMP']}/boxstarter/setup.bat"
-  not_if { ::Dir.exist?(::File.join(ENV['AppData'], 'boxstarter')) }
+  command "#{node['boxstarter']['tmp_dir']}/setup.bat"
 end
