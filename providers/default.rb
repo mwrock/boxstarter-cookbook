dd2afe158a6ec4932f859961e3e9d6d46b48f8e6
mwrock <matt@mattwrock.com>
2014-07-12 11:32:14 -0700

    create a scheduled task if remote and throw back errors

--- providers/default.rb
+++ providers/default.rb
@@ -12,6 +12,7 @@ action :run do
   code = @new_resource.code || @new_resource.script
   password = @new_resource.password
   disable_reboots = @new_resource.disable_reboots
+  disable_boxstarter_restart = @new_resource.disable_boxstarter_restart
   start_chef_client_onreboot = @new_resource.start_chef_client_onreboot
   script_path = "#{node['boxstarter']['tmp_dir']}/package.ps1"
 
@@ -32,6 +33,8 @@ action :run do
     cookbook "boxstarter"
     variables({
       :password => password,
+      :disable_boxstarter_restart => disable_boxstarter_restart,
+      :is_remote => running_remote(Process.ppid)
       :temp_dir => node['boxstarter']['tmp_dir'],
       :disable_reboots => disable_reboots
     })
@@ -77,6 +80,38 @@ def child_of_boxstarter(parent)
 
   if !proc.CommandLine.nil? && proc.CommandLine.downcase.include?('boxstarter')
     Chef::Log.info "***Found boxstarter parent pid #{parent}...returning true***"
+    return true 
+  end
+
+  if proc.Name == 'services.exe'
+    Chef::Log.info "***Proc was running #{proc.Name}...quitting since this is the effective trunk***"
+    return false
+  end
+
+  Chef::Log.info "***Proc was running #{proc.Name}...trying its parent***"
+  return child_of_boxstarter(proc.ParentProcessID)
+end
+
+def running_remote(parent)
+  Chef::Log.info "***Looking for winrs parents at pid #{parent}***"
+
+  if parent.nil?
+    Chef::Log.info "***No more parents. Finished looking for winrs parents***"
+    return false 
+  end
+
+  wmi = WIN32OLE.connect("winmgmts://")
+  parent_proc = wmi.ExecQuery("Select * from Win32_Process where ProcessID=#{parent}")
+  
+  if parent_proc.each.count == 0
+    Chef::Log.info "***No process for pid #{parent}. Finished looking for boxstarter parents***"
+    return false
+  end
+
+  proc = parent_proc.each.next
+
+  if proc.Name == "winrshost.exe"
+    Chef::Log.info "***Found winrs parent pid #{parent}...returning true***"
     return true 
   end
 
