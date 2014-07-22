6bf00bfe79299e2b7dc4ad4eb8839e08dc5ed49b
Matt Wrock <matt@mattwrock.com>
2014-07-22 04:50:02 -0700

    ensure a failed boxstarter run sends an error code and has its output written

--- test/cookbooks/boxstarter_test/recipes/simple.rb
+++ test/cookbooks/boxstarter_test/recipes/simple.rb
@@ -3,7 +3,7 @@ include_recipe 'boxstarter::default'
 boxstarter "Simple Package" do
   disable_reboots true
   code <<-EOH
-    Set-WindowsExplorerOptions -EnableShowHidenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+    Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
     cinst console2
   EOH
 end
